7e2c5f9ef7d3a71dcf7e7b6999ff8ce1dda924dd
Lauro Pereira <lauro.s.pereira@gmail.com>
2016-06-25 13:02:33 -0300

    implementação do metodo select($map) do objeto tipoProdutoDto.

--- TrabalhoFinalWeb/database/model/TipoProdutoDto.php
+++ TrabalhoFinalWeb/database/model/TipoProdutoDto.php
@@ -30,16 +30,42 @@ class TipoProdutoDto implements Dto{
         
         return $resultMap;
     }
-    function select($map){
-        /*
-         * @todo implementar metodo select por campo
-         */
+    
+    private function preparaSql($array){
+        $sql = "SELECT * FROM tipo WHERE 1 = 1 ";
+        
+        if(isset($array["ds_tipo"]) && !empty(preg_replace('/[ \t\n\r\f\v]/i', '', $array["ds_tipo"]))){
+            $sql .= "AND ds_tipo like '%" . preg_replace('/[ \t\n\r\f\v]/i', '', $array["ds_tipo"]) . "%' ";
+        }
+        
+        return $sql;
     }
+    
+    function select($map){
+        $resultMap = array();
+        
+        $sql = $this->preparaSql($map);
+        $return = $this->con->query($sql);
+
+        while($result = $return->fetch_assoc()){
+            
+            $tipoProduto = new TipoProduto();
+            
+            $tipoProduto->setId($result["id_tipo"]);
+            $tipoProduto->setTipoProduto($result["ds_tipo"]);
+            
+            array_push($resultMap, $tipoProduto);
+        }
+        
+        return $resultMap;
+    }
+    
     function count(){
         /*
          * @todo implementar metodo count
          */
     }
+    
     function find($id){
         $sql = "SELECT * FROM tipo WHERE id_tipo = ". $id;
         
